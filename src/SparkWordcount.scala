71d0b535bd75a87da42e6a27c9f2b30192ca8082
Heinrich Latreider <heinrich.latreider@live.de>
2017-04-17 19:17:02 +0200

    Wordcount Example with Spark and Scala

---
+++ src/SparkWordcount.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.SparkContext 
+import org.apache.spark.SparkContext._ 
+import org.apache.spark._  
+
+object SparkWordCount { 
+   def main(args: Array[String]) { 
+
+      val sc = new SparkContext( "local", "Word Count", "/usr/local/spark") 
+		
+      /* local = master URL; Word Count = application name; */  
+      /* /usr/local/spark = Spark Home; Nil = jars; Map = environment */ 
+      /* Map = variables to work nodes */ 
+      /*creating an inputRDD to read text file (in.txt) through Spark context*/ 
+      val input = sc.textFile("/home/heinrich/Schreibtisch/input.txt") 
+      /* Transform the inputRDD into countRDD */ 
+		
+      val count = input.flatMap(line ⇒ line.split(" ")) 
+      .map(word ⇒ (word, 1)) 
+      .reduceByKey(_ + _) 
+       
+      /* saveAsTextFile method is an action that effects on the RDD */  
+      count.foreach(word => println(word._1 + " " + word._2))
+      //count.saveAsTextFile("outfile") 
+      System.out.println("OK"); 
+   } 
+} 
